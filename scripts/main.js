2095f2254c56a2d15170c6dea264ed2a8f552247
Nikolai-Grytvik-Borbe <nikolaiborbe@gmail.com>
2024-05-02 18:50:48 +0200

    .

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,6 @@ document.addEventListener('keydown', function (event) {
         const input = Array.from(document.querySelectorAll('input')).filter(x => x.offsetWidth > 0 && x.offsetHeight > 0 && ['email', 'date', 'datetime-local', 'number', 'password', 'search', 'tel', 'text', 'time', 'url'].includes(x.type));
 	let search;
 
-	// 1 and 0 inputs cases
 	if (input.length === 1) {
 		input[0].focus();
 		console.debug('1 focused ', input[0])
@@ -11,22 +10,11 @@ document.addEventListener('keydown', function (event) {
 	}
 
 	if (input.length) {
-		// type === 'search'
 		search = input.filter(x => x.type === 'search');
-
-		// id === 'search'
 		if (!search.length) search = input.filter(x => x.id === 'search');
-
-		// id contains 'search'
 		if (!search.length) search = input.filter(x => x.id.toLowerCase().indexOf('search') !== -1);
-
-		// classes contain 'search'
 		if (!search.length) search = input.filter(x => x.className.toLowerCase().indexOf('search') !== -1);
-
-		// outerHTML contains 'search'
 		if (!search.length) search = input.filter(x => x.outerHTML.toLowerCase().indexOf('search') !== -1);
-
-		// parents' ids contain 'search'
 		if (!search.length) search = input.filter(x => {
 			do {
 				x = x.parentElement;
@@ -34,8 +22,6 @@ document.addEventListener('keydown', function (event) {
 			} while (x.parentElement !== document.body)
 			return false;
 		});
-
-		// if something found, focus
 		if (search.length) {
 			// activate first (random) element in input (if exists)
 			search.sort((x, y) => x.childElementCount < y.childElementCount ? -1 : 1);
@@ -45,25 +31,12 @@ document.addEventListener('keydown', function (event) {
 		}
 
 	}
-
-	// look for non-input elements
 	let inputAll = Array.from(document.querySelectorAll('*')).filter(x => x.offsetWidth > 0 && x.offsetHeight > 0);
-
-	// non-input elements, id === 'search'
 	search = inputAll.filter(x => x.id === 'search');
-
-	// non-input elements, id contains 'search'
 	if (!search.length) search = inputAll.filter(x => x.id.toLowerCase().indexOf('search') !== -1);
-
-	// non-input elements, classes contains 'search'
 	if (!search.length) search = inputAll.filter(x => x.className.toLowerCase().indexOf('search') !== -1);
-
-	// non-input elements, outerHTML contains 'search'
 	if (!search.length) search = inputAll.filter(x => x.outerHTML.toLowerCase().indexOf('search') !== -1);
-
-	// click first (random) element in input (if exists)
 	if (search.length) {
-		//sort search array
 		search.sort((x, y) => x.childElementCount < y.childElementCount ? -1 : 1);
 		search[0].click();
 		console.debug('clicked ', search[0])
